d43c873484d5df1a28100bbd72bbc36dd0d8c696
yohanis-ke <yohanes.ke@gmail.com>
2018-08-27 21:03:02 -0400

    I write the code for the main class

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+class Calculator
+
+  def add(num1, num2=0)
+    return num1+num2
+  end
+
+  def subtract(num1, num2)
+    return num1 - num2
+  end
+
+  def sum(array = [])
+    return array.sum
+  end
+
+end
